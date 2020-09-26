8c545b14ec5d1e2b637a6642d03e6163c36e8f7a
paul-emilemorgades <paul-emile.morgades@epita.fr>
2020-09-26 14:31:56 +0200

    [WORKSHOP] notation compliant to unwritten function

--- Workshop/WP1/notation.py
+++ Workshop/WP1/notation.py
@@ -42,107 +42,131 @@ except:
 
 #test compute matrix
 
-m = [[0,1],[1,0]]
-arr = [[0., 1., 0., 0.],
- [1., 0., 0., 0.],
- [0., 0., 0., 1.],
- [0., 0., 1., 0.]]
-t3_1 = np.array_equal(computeMatrix(m,2,0), arr)
-
-m = [[0,1],[1,0]]
-arr = [[0., 0., 1., 0.],
- [0., 0., 0., 1.],
- [1., 0., 0., 0.],
- [0., 1., 0., 0.]]
-t3_2 = np.array_equal(computeMatrix(m,2,1), arr)
-
-t3 = t3_1 and t3_2
-
+def testComputeMatrix():
+    m = [[0,1],[1,0]]
+    arr = [[0., 1., 0., 0.],
+     [1., 0., 0., 0.],
+     [0., 0., 0., 1.],
+     [0., 0., 1., 0.]]
+    t3_1 = np.array_equal(computeMatrix(m,2,0), arr)
+    m = [[0,1],[1,0]]
+    arr = [[0., 0., 1., 0.],
+     [0., 0., 0., 1.],
+     [1., 0., 0., 0.],
+     [0., 1., 0., 0.]]
+    t3_2 = np.array_equal(computeMatrix(m,2,1), arr)
+    return t3_1 and t3_2
+t3 = False
+try:
+    t3 = testComputeMatrix()
+except:
+    pass
 #test not gate
 
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,0)])
-t4_1 = np.array_equal(arr,[0., 1., 0., 0.])
+def testNotgate():
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,0)])
+    t4_1 = np.array_equal(arr,[0., 1., 0., 0.])
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,1)])
+    t4_2 = np.array_equal(arr,[0., 0., 1., 0.])
+    arr =quantumComputer(3,[QuantumGate(TypeOfQuantumGate.NOT,0)])
+    t4_3 = np.array_equal(arr,[0., 1., 0., 0.,0., 0., 0., 0.])
+    return t4_1 and t4_2 and t4_3
 
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,1)])
-t4_2 = np.array_equal(arr,[0., 0., 1., 0.])
-
-arr =quantumComputer(3,[QuantumGate(TypeOfQuantumGate.NOT,0)])
-t4_3 = np.array_equal(arr,[0., 1., 0., 0.,0., 0., 0., 0.])
-
-t4 = t4_1 and t4_2 and t4_3
+t4 = False
+try:
+    t4 = testNotgate()
+except:
+    pass
 
 #test hadamard gate
 
-arr =quantumComputer(1,[QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
-t5_1 = (np.isclose(arr,[1/math.sqrt(2), 1/math.sqrt(2) ]).all()) 
+def testHadamardGate():
+    arr =quantumComputer(1,[QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
+    t5_1 = (np.isclose(arr,[1/math.sqrt(2), 1/math.sqrt(2) ]).all()) 
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
+    t5_2 = (np.isclose(arr,[1/math.sqrt(2), 1/math.sqrt(2),0.,0. ]).all()) 
+    return t5_1 and t5_2
 
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
-t5_2 = (np.isclose(arr,[1/math.sqrt(2), 1/math.sqrt(2),0.,0. ]).all()) 
-
-t5 = t5_1 and t5_2
+t5 = False
+try:
+    t5 = testHadamardGate()
+except:
+    pass
 
 #test multi gate
 
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.HADAMARD,0),QuantumGate(TypeOfQuantumGate.HADAMARD,1)])
-t6_1 = (np.isclose(arr,[1/math.sqrt(4), 1/math.sqrt(4),1/math.sqrt(4),1/math.sqrt(4) ]).all())
-
-inp = [QuantumGate(TypeOfQuantumGate.NOT,2),QuantumGate(TypeOfQuantumGate.NOT,1) , QuantumGate(TypeOfQuantumGate.NOT,2)]
-arr1 = quantumComputer(3,inp)
-t6_2 = np.array_equal(arr1,[0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j])
-
-inp = [QuantumGate(TypeOfQuantumGate.NOT,2),QuantumGate(TypeOfQuantumGate.NOT,1) , QuantumGate(TypeOfQuantumGate.NOT,2),QuantumGate(TypeOfQuantumGate.HADAMARD,0)]
-arr1 = quantumComputer(3,inp)
-result = [0. +0.j, 0.+0.j, 0.70710678+0.j, 0.70710678+0.j,
- 0.  +0.j, 0. +0.j ,0.+0.j, 0.+0.j]
-t6_3 = (np.isclose(arr1,result).all())
-
-t6 = t6_1 and t6_2 and t6_3
+def testMultiGate():
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.HADAMARD,0),QuantumGate(TypeOfQuantumGate.HADAMARD,1)])
+    t6_1 = (np.isclose(arr,[1/math.sqrt(4), 1/math.sqrt(4),1/math.sqrt(4),1/math.sqrt(4) ]).all())
+    inp = [QuantumGate(TypeOfQuantumGate.NOT,2),QuantumGate(TypeOfQuantumGate.NOT,1) , QuantumGate(TypeOfQuantumGate.NOT,2)]
+    arr1 = quantumComputer(3,inp)
+    t6_2 = np.array_equal(arr1,[0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j])
+    inp = [QuantumGate(TypeOfQuantumGate.NOT,2),QuantumGate(TypeOfQuantumGate.NOT,1) , QuantumGate(TypeOfQuantumGate.NOT,2),QuantumGate(TypeOfQuantumGate.HADAMARD,0)]
+    arr1 = quantumComputer(3,inp)
+    result = [0. +0.j, 0.+0.j, 0.70710678+0.j, 0.70710678+0.j,
+     0.  +0.j, 0. +0.j ,0.+0.j, 0.+0.j]
+    t6_3 = (np.isclose(arr1,result).all())
+    return t6_1 and t6_2 and t6_3
+t6 = False
+try:
+    t6 = testMultiGate()
+except:
+    pass
 
 #test ComputeCnot
 
-arr = [[1., 0., 0., 0.],
- [0., 0., 0., 1.],
- [0., 0., 1., 0.],
- [0., 1., 0., 0.]]
+def testComputeCNOT():
+    arr = [[1., 0., 0., 0.],
+     [0., 0., 0., 1.],
+     [0., 0., 1., 0.],
+     [0., 1., 0., 0.]]
+    t7_1 = np.array_equal(computeCNOT(2,0,1) , arr)
+    arr = [[1., 0., 0., 0.],
+     [0., 1., 0., 0.],
+     [0., 0., 0., 1.],
+     [0., 0., 1., 0.]]
+    t7_2 = np.array_equal(computeCNOT(2,1,0) , arr)
+    return  t7_1 and t7_2
 
-t7_1 = np.array_equal(computeCNOT(2,0,1) , arr)
-
-
-arr = [[1., 0., 0., 0.],
- [0., 1., 0., 0.],
- [0., 0., 0., 1.],
- [0., 0., 1., 0.]]
-
-t7_2 = np.array_equal(computeCNOT(2,1,0) , arr)
-
-
-t7 = t7_1 and t7_2
+t7 = False
+try:
+    t7 = testComputeCNOT()
+except:
+    pass
 #test CNOT
 
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,1),QuantumGate(TypeOfQuantumGate.CNOT,1,0)])
-t8_1  = (np.array_equal(arr , [0,0,0,1]))
+def testCNOT():
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,1),QuantumGate(TypeOfQuantumGate.CNOT,1,0)])
+    t8_1  = (np.array_equal(arr , [0,0,0,1]))
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,1),QuantumGate(TypeOfQuantumGate.CNOT,0,1)])
+    t8_2  = (np.array_equal(arr , [0,0,1,0]))
+    arr =quantumComputer(3,[QuantumGate(TypeOfQuantumGate.NOT,0),QuantumGate(TypeOfQuantumGate.CNOT,0,1),QuantumGate(TypeOfQuantumGate.CNOT,1,2)])
+    res = np.zeros([8])
+    res[7] = 1
+    t8_3  = (np.array_equal(arr , res))
+    return t8_1 and t8_2 and t8_3
 
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,1),QuantumGate(TypeOfQuantumGate.CNOT,0,1)])
-t8_2  = (np.array_equal(arr , [0,0,1,0]))
-
-arr =quantumComputer(3,[QuantumGate(TypeOfQuantumGate.NOT,0),QuantumGate(TypeOfQuantumGate.CNOT,0,1),QuantumGate(TypeOfQuantumGate.CNOT,1,2)])
-res = np.zeros([8])
-res[7] = 1
-t8_3  = (np.array_equal(arr , res))
-
-t8 = t8_1 and t8_2 and t8_3
+t8 = False
+try:
+    t8 = testCNOT()
+except:
+    pass
 
 #test compute proba
 
-arr =quantumComputer(1,[QuantumGate(TypeOfQuantumGate.NOT,0),
-                        QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
-t9_1 = (np.isclose(computeProbability(arr),[0.5,0.5]).all())
-
-arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,0),
-                        QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
-t9_2 = (np.isclose(computeProbability(arr),[0.5,0.5,0.,0.]).all())
-
-t9 = t9_1 and t9_2
+def testComputeProba():
+    arr =quantumComputer(1,[QuantumGate(TypeOfQuantumGate.NOT,0),
+                            QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
+    t9_1 = (np.isclose(computeProbability(arr),[0.5,0.5]).all())
+    arr =quantumComputer(2,[QuantumGate(TypeOfQuantumGate.NOT,0),
+                            QuantumGate(TypeOfQuantumGate.HADAMARD,0)])
+    t9_2 = (np.isclose(computeProbability(arr),[0.5,0.5,0.,0.]).all())
+    return t9_1 and t9_2
+t9 = False
+try:
+    t9 = testComputeProba()
+except:
+    pass
 
 mark = 0
 if(t1):
