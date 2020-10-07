7e5148d12578df293b0c4fcf01187de7ece1631e
paul-emilemorgades <paul-emile.morgades@epita.fr>
2020-10-07 14:55:57 +0200

    Workshop: add firstversion of the wp2 example

---
+++ Workshop/WP2/correction/example.py
@@ -0,0 +1,23 @@
+import math 
+import numpy as np 
+from qiskit import QuantumCircuit, execute, Aer
+
+circuit = QuantumCircuit(1)
+circuit.h(0)
+sim = Aer.get_backend('statevector_simulator')
+job = execute(circuit, sim)
+
+arr = job.result().get_statevector(circuit)
+
+print(circuit.draw())
+assert(np.isclose(arr,[1/math.sqrt(2), 1/math.sqrt(2) ]).all())    
+circuit.measure_all()
+simulator = Aer.get_backend('qasm_simulator')
+# Execute the circuit on the qasm simulator
+job = execute(circuit, simulator, shots=1000)
+# Grab results from the job
+result = job.result()
+# Returns counts 
+counts = result.get_counts(circuit) 
+print(counts)
+
